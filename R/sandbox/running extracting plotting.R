45893cfa599a4426d6bc13b6f9ccd3c7d708dcb7
NeilHobbs <neilphobbs@hotmail.com>
2021-08-04 10:22:33 +0100

    code to run and plot to test simulations

--- R/sandbox/running extracting plotting.R
+++ R/sandbox/running extracting plotting.R
@@ -1,14 +1,14 @@
-A = run_mosaic_simulation(number.of.tiles.width = 11,
-                          number.of.tiles.length = 11,
-                          number.of.insecticides = 1,
-                          min.male.exposure = 0.7,
-                          max.male.exposure = 0.7,
-                          min.female.exposure = 0.7,
-                          max.female.exposure = 0.7,
-                          min.heritability = 0.2,
-                          max.heritability = 0.2,
-                          min.dispersal = 0.1,
-                          max.dispersal = 0.1,
+A = run_mosaic_simulation(number.of.tiles.width = 3,
+                          number.of.tiles.length = 1,
+                          number.of.insecticides = 2,
+                          min.male.exposure = 0.8,
+                          max.male.exposure =0.8,
+                          min.female.exposure = 0.9,
+                          max.female.exposure = 0.9,
+                          min.heritability = 0.3,
+                          max.heritability = 0.3,
+                          min.dispersal = 0.3,
+                          max.dispersal = 0.3,
                           exposure.scaling.factor = 10,
                           min.fitness.cost = 0.01,
                           max.fitness.cost = 0.01,
@@ -21,9 +21,9 @@ A = run_mosaic_simulation(number.of.tiles.width = 11,
 
 #xcoord , ycoord , generation , insecticide deployed , insecticide tracked, resistance
 
-final_df = get_simulation_dataframe_mosaics(number.of.tiles.width = 11,
-                                            number.of.tiles.length = 11,
-                                            number.of.insecticides = 1,
+final_df = get_simulation_dataframe_mosaics(number.of.tiles.width = 3,
+                                            number.of.tiles.length = 1,
+                                            number.of.insecticides = 2,
                                             maximum.generations = 200,
                                             simulation.output = A)
 
@@ -85,6 +85,39 @@ animation.plot = ggplot(final_df3, aes(x=xcoord,
   labs(title = 'generation: {closest_state}')+
   ease_aes('linear')
 
-animate(animation.plot, renderer = gifski_renderer())
+animate(animation.plot)
+
+
+plot.list = list()
+for(i in 1:200){
+  final_df4 = final_df3%>%
+    dplyr::filter(generation == i)
+  
+  
+  plot.list[[i]] = ggplot(final_df4, aes(x=xcoord,
+                                       y=ycoord))+
+  geom_tile(aes(fill = resistance),
+            colour = "white") +
+  xlab("x coordinate")+
+  ylab("y coordinate")+
+  geom_text(aes(label = insecticide.deployed), colour = "black") +
+  scale_fill_gradientn(colours = rev(c("#d73027", "#f46d43", "#fdae61", "#abd9e9", "#74add1", "#4575b4")),  limits=c(0,130))+
+  theme_bw()
+
+}
+
+plot.list[[200]]
+
+plot.list[[10]]
+
+
+
+for (i in 1:200) {
+  file_name = paste("generation", i, ".tiff", sep="")
+  tiff(file_name)
+  print(plot.list[[i]])
+  dev.off()
+}
+
 
 
