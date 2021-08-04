d73e975c326cf5687813e0336f5c68811ae1119b
NeilHobbs <neilphobbs@hotmail.com>
2021-08-04 10:21:47 +0100

    added annotation and comments

--- R/adult_population_size_after_suppression.R
+++ R/adult_population_size_after_suppression.R
@@ -1,3 +1,15 @@
+#'@title Calculate the mosquito population size after insecticide induced suppression
+#'
+#'@param population.size.landscape = The matrix holding the population sizes of the tiles in landscape
+#'@param deployment.landscape = The matrix holding which insecticide is deployed in each tile
+#'@param x.coord = The x coordinate of the tracked tile
+#'@param y.corrd = The y coordinate of the tracked tile
+#'@param sim.array = The array which holds the simulation data
+#'@param conversion.factor = The regression coefficient between bioassay survival and field survival
+#'@param intercept = The regression intercept between bioassay survival and field survival
+#'@param half.population.bioassay.survival.resistance = The polygenic resistance score which gives 50% bioassay survival
+#'@param female.exposure.landscape = The matrix holding the female exposure values
+
 adult_population_size_after_suppression = function(population.size.landscape,
                                                    deployment.landscape,
                                                    x.coord,
@@ -11,7 +23,7 @@ adult_population_size_after_suppression = function(population.size.landscape,
 
   #which insecticide:
   deployed = deployment.landscape[x.coord, y.coord]
-if(deployed != 0){
+if(deployed != 0){#suppression only occurs in insecticide treated locations
   resistance = sim.array[x.coord, y.coord, deployed, generation-1, "migration"] #resistance from previous generation
 
   survival = convert_bioassay_survival_to_field(bioassay.survival = resistance_to_bioassay_survival(maximum.bioassay.survival.proportion = 1,
@@ -29,7 +41,7 @@ if(deployed != 0){
 
   final.population.size = (population.size*female.exposure*survival) + ((1-female.exposure)*population.size)
 }
-  if(deployed == 0){final.population.size = population.size = population.size.landscape[x.coord, y.coord]}
+  if(deployed == 0){final.population.size = population.size.landscape[x.coord, y.coord]}
 
   return(final.population.size)
 }
