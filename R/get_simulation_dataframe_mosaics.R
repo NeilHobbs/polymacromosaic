6aab60529269ca64741f7b68e1702f223d0e03aa
NeilHobbs <neilphobbs@hotmail.com>
2021-09-24 16:11:03 +0100

    corrected x y

--- R/get_simulation_dataframe_mosaics.R
+++ R/get_simulation_dataframe_mosaics.R
@@ -12,22 +12,22 @@ get_simulation_dataframe_mosaics = function(number.of.tiles.width,
   Deployed = simulation.output[[2]][[1]]
 
   for(insecticide in 1:number.of.insecticides){
-    for(x in 1:number.of.tiles.width){
-      for(y in 1:number.of.tiles.length){
-        resistance = simulation.array[x, y, insecticide, , "migration"]
+    for(y in 1:number.of.tiles.length){
+      for(x in 1:number.of.tiles.width){
+        resistance = simulation.array[y, x, insecticide, , "migration"]
         ycoord = y
         xcoord= x
         generation = seq(1, maximum.generations, by = 1)
         insecticide.tracked = insecticide
-        insecticide.deployed = rep(Deployed[x, y], maximum.generations)
+        insecticide.deployed = rep(Deployed[y, x], maximum.generations)
         df = data.frame(resistance, ycoord, xcoord, generation, insecticide.tracked, insecticide.deployed)
 
-        temp.list.y[[y]] = df
+        temp.list.x[[x]] = df
       }
-      temp.list.x[[x]] = do.call(rbind, temp.list.y)
+      temp.list.y[[y]] = do.call(rbind, temp.list.x)
     }
 
-    temp.list.insecticide[[insecticide]] = do.call(rbind, temp.list.x)
+    temp.list.insecticide[[insecticide]] = do.call(rbind, temp.list.y)
   }
 
   final_df = do.call(rbind, temp.list.insecticide)
